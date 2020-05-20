a744cb5d1479a5dc1bc7004d25500a9d2ee4996c
ClaraCordeiro <51731081+ClaraCordeiro@users.noreply.github.com>
2020-05-20 21:07:06 +0100

    Update stlfit.R

--- stlfit.R
+++ stlfit.R
@@ -1,18 +1,20 @@
 stl.fit<-function(y,rob,k){
+  
+  ## nextodd from stl() 
   nextodd <- function(x) {
     x <- round(x)
     if (x%%2 == 0) 
       x <- x + 1
     as.integer(x)
   }
-  casa<-c()
+  aux<-c()
   fit<- stl(y, s.window ="periodic",robust=rob)
   d1<-fit$time.series[,"seasonal"]
   d2<-fit$time.series[,"trend"]
   fit1<-d1+d2
   m1<-accuracy(y,fit1)[k] 
-  casa$measure<-m1
-  casa$stlfit<-fit
+  aux$measure<-m1
+  aux$stlfit<-fit
   for (i in 7:length(y)){
     t.win<-nextodd(ceiling(1.5*frequency(y)/(1-1.5/i)))
     for (kk in t.win:length(y)){
@@ -25,7 +27,7 @@ stl.fit<-function(y,rob,k){
           m2<-accuracy(y,fit2)[k] 
           if(m2<m1){
             m1<-m2
-            casa$measure<-m1
-            casa$stlfit<-fit3
+            aux$measure<-m1
+            aux$stlfit<-fit3
           }}}}}
-  casa}
+  aux}
